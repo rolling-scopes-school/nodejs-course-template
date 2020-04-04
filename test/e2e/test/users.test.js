c106660ab4c26ae2a78fef03742e290a579d854b
Mikhail Kedzel <mishanx873@gmail.com>
2020-04-04 21:09:39 +0300

    fix: find user by id test first creates a mock user

--- test/e2e/test/users.test.js
+++ test/e2e/test/users.test.js
@@ -35,6 +35,12 @@ describe('Users suite', () => {
 
     it('should get a user by id', async () => {
       // Setup:
+      //Create the user
+      await request
+          .post(routes.users.create)
+          .set('Accept', 'application/json')
+          .send(TEST_USER_DATA);
+
       const usersResponse = await request
         .get(routes.users.getAll)
         .set('Accept', 'application/json')
@@ -51,6 +57,9 @@ describe('Users suite', () => {
 
       expect(userResponse.body).to.be.instanceOf(Object);
       expect(userResponse.body.id).to.equal(userId);
+
+      // Clean up, delete the user we created
+      await request.delete(routes.users.delete(userId));
     });
   });
 
