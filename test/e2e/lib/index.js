e09d0c748bb941c89d64b426f0d9f0ef317a5038
Net-zen <prankster@myttk.ru>
2020-10-15 12:32:02 +0600

    fix: port for tests now depends of port in .env

--- test/e2e/lib/index.js
+++ test/e2e/lib/index.js
@@ -1,9 +1,19 @@
 const supertest = require('supertest');
 const debug = require('debug')('rs:lib');
 
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+  path: path.join(__dirname, '../../../.env')
+});
+
 const routes = require('./routes');
 
-const host = process.env.HOST || 'localhost:4000';
+const host =
+  process.env.HOST || process.env.PORT
+    ? `localhost:${process.env.PORT}`
+    : 'localhost:4000';
 debug('HOST', host);
 
 const request = supertest(host);
