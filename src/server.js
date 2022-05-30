ea8ed46f596acf4c0451021c8bd440bb416573c2
Imamov Erzhigit <erjigit17@gmail.com>
2022-05-30 15:01:54 +0600

    disable Eslint warning "unexpected console statement"

--- src/server.js
+++ src/server.js
@@ -2,5 +2,6 @@ const { PORT } = require('./common/config');
 const app = require('./app');
 
 app.listen(PORT, () =>
+  // eslint-disable-next-line no-console
   console.log(`App is running on http://localhost:${PORT}`)
 );
