a1d2dfc7474759c16bda3ab423c50e3bdc174672
Henadzi Shutko <hazardsoft@users.noreply.github.com>
2023-09-29 17:16:48 +0300

    test: add tests for "/refresh" endpoint (#78)
    details: Jest config removed from package.json file and placed to the root (Jest finds it by default)

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,5 +1,11 @@
 import getTokenAndUserId from './getTokenAndUserId';
 import removeTokenUser from './removeTokenUser';
 import shouldAuthorizationBeTested from './shouldAuthorizationBeTested';
+import generateRefreshToken from './tokens';
 
-export { getTokenAndUserId, shouldAuthorizationBeTested, removeTokenUser };
+export {
+  getTokenAndUserId,
+  shouldAuthorizationBeTested,
+  removeTokenUser,
+  generateRefreshToken,
+};
